d5610088fa2a2ef5d60a00b65505e64613e49146
Ashley Coleman <ashley@yieldify.com>
2015-08-28 14:39:14 +0100

    update: React props

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,20 @@ module.exports = function (modules, document) {
     var React = modules.React;
     // React = require("react");
 
+    var LikeList = React.createClass({
+        render: function () {
+            return (
+            <ol>
+                <li>{this.props.text}</li>
+            </ol>
+            )
+        }
+    });
     var App  = React.createClass({
         render: function () {
-            return <h2>
-                    Hello there ok awesome!!
-                    <div onClick= {this.handleClick.bind(null, 'Bang', 'I shot my lover down')}>XXX</div>
-                    <div onClick= {this.handleClick.bind(null, 'Boom', '( there goes the )')}>XXX</div>
-                </h2>
+            return <div>
+                    <LikeList text="turtles"/>
+                </div>
         },
         handleClick: function (bound1, bound2, e) {
             e.target.textContent = [bound1, bound2].join(', ');
