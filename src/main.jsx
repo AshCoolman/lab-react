481e30537f830bf699fefd778817f3a5420314f0
Ashley Coleman <ashley@yieldify.com>
2015-08-30 16:42:34 +0100

    update: regenerate state

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 console.clear();
-console.log('ok');
 module.exports = function (modules, document) {
     var React = modules.React;
 
@@ -13,25 +12,48 @@ module.exports = function (modules, document) {
                 ]
             }
         },
+        generatePeopleCsv: function () {
+
+            var getRandom = function (names) {
+                return names[Math.round(names.length * Math.random())];
+            },
+            getRandomName = function () {
+                return getRandom("Gary,Barry,Stephen,James".split(','));
+            },
+            getRandomColour = function () {
+                return getRandom("red,salmon,aquamarine,gold,orange,brown,black".split(','));
+            };
+
+            var people = [];
+            for (var p = 0; p < 4; p++) {
+                people.push([
+                    getRandomName(),
+                    getRandomColour(),
+                    getRandomColour()
+                ].join(','));
+            }
+            this.setState({peopleCsv:people});
+        },
         render: function () {
             if (!this.state['peopleCsv']) {
                 return <p>You dun fucked up son</p>
             }
             return (
                 <div>
+                    <div>
+                        <button onClick={this.generatePeopleCsv}>generatePeopleCsv</button>
+                    </div>
                 {
 
                     this.state.peopleCsv.map( function (e, i, a) {
                         var name, top, bottom;
                         [ name, top, bottom ] = e.split(',');
-                        console.log([name, top, bottom]);
                         return <div>
                             <p>
                                 <i style={{color: 'grey'}}>{ name }</i>
                                 <div style={{color: top }}>top</div>
                                 <div style={{color: bottom }}>bottom</div>
                             </p>
-
                         </div>
                     })
                 }
